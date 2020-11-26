f6a08fa65b769bd07699c446aabb91328219f921
Gaetandroid <73991618+Gaetandroid@users.noreply.github.com>
2020-11-26 11:31:31 +0100

    Update 25.py

---
+++ 25.py
@@ -0,0 +1,13 @@
+#Es. 2
+print("Inserire i voti e il nome del primo candidato")
+v1 = int(input())
+n1 = input()
+print("Inserire i voti e il nome del secondo candidato")
+v2 = int(input())
+n2 = input()
+lista1 = [n1, n2]
+lista2 = [v1, v2]
+lista1.sort()
+lista2.sort(reverse=True)
+print(lista1)
+print(lista2)
