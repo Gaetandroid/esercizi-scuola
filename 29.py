115865639b6e9eaf0e4ead416b962b17562c0b41
Gaetandroid <73991618+Gaetandroid@users.noreply.github.com>
2020-12-02 09:08:05 +0100

    Update 29.py

--- 29.py
+++ 29.py
@@ -1,13 +1,11 @@
 from random import randint
 ciclo = True
-ciclo2 = True
 città = []
 ltmax = []
 ltmin = []
 n = 0
 escursione_max = randint(1, 100)
 while ciclo == True:
-    ciclo2 = True
     n += 1
     print("Digita nome della città numero", n)
     nomec = input()
@@ -24,8 +22,8 @@ while ciclo == True:
     else:
         pass
 
-ciclo3 = True
-while ciclo3 == True:
+ciclo2 = True
+while ciclo2 == True:
     print("\nPremi 'c' per vedere una lista delle città inserite. \nPremi 'max' per vedere una lista dei valori massimi dell'escursione termica inseriti. \nPremi 'min' per vedere una lista dei valori minimi dell'escursione termica inseriti. \nPremi 'q' per uscire. \nPremi qualsiasi altro tasto per vedere il limite del contatore e le città che NON lo rispettano")
     output = input()
     if output == "c":
@@ -35,10 +33,10 @@ while ciclo3 == True:
     elif output == "min":
         print("\nValori minimi escursione termica: ", ltmin)
     elif output == "q":
-        ciclo3 = False
+        ciclo2 = False
     else:
-        ciclo4 = True
-        while ciclo4 == True:
+        ciclo3 = True
+        while ciclo3 == True:
             print("Scegli un indice esistente nelle 3 liste per selezionare una città e la sua escursione termica")
             indicef = int(input())
             escursione_termica = ltmax[indicef] - ltmin[indicef]
@@ -46,22 +44,22 @@ while ciclo3 == True:
             print("\nValore massimo del contatore: ", escursione_max)
             if diffesc > 0:
                 print("\nL'escursione termica di", città[indicef],"è di", escursione_termica,"rientra nel range prefissato per", diffesc,"gradi")
-                ciclo4 = False
+                ciclo3 = False
             else:
                 print("\nL'escursione termica di", città[indicef],"è di", escursione_termica,"non rientra nel range prefissato per", diffesc,"gradi")
-                ciclo5 = True
-                while ciclo5 == True:
+                ciclo4 = True
+                while ciclo4 == True:
                     print("\nSto alzando il range del contatore per far entrare l'escursione termica di", città[indicef])
                     diffesc += 1
                     if diffesc > 0:
                         print("\nOra i valori sono stabilizzati! Premi 't' per tornare al menù principale, premi qualsiasi altro tasto per chiudere.")
                         finale = input()
                         if finale == "t":
-                            ciclo4 = False
-                            ciclo5 = False
-                        else:
                             ciclo3 = False
                             ciclo4 = False
-                            ciclo5 = False
+                        else:
+                            ciclo2 = False
+                            ciclo3 = False
+                            ciclo4 = False
                     else:
                         pass
