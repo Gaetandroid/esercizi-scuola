0f63b9841a91cf44bbafa00d84777e3b0382b5f5
Gaetandroid <citarellagaetano26@gmail.com>
2020-12-02 14:05:43 +0100

    3/12

--- 30.py
+++ 30.py
@@ -1,1 +1,22 @@
-niente
+numero = []
+print("Inserisci la lunghezza del numero binario")
+lunghezza = int(input())
+ripetizione = 0
+for i in range(lunghezza):
+    ripetizione += 1
+    print("A partire da destra, inserisci la cifra n", ripetizione)
+    cif = int(input())
+    numero.append(cif)
+numero.reverse()
+print("\nNumero binario registrato diviso in cifre da 1: ",numero)
+somma = 0
+potenza = 0
+for i in range(lunghezza):
+    finale = numero[lunghezza-1]
+    if finale == 0:
+        somma += 0
+    elif finale == 1:
+        somma += 2**potenza
+    lunghezza -= 1
+    potenza += 1
+print("\nNumero decimale ottenuto: ",somma)
