172d62ded9b2030c8c7d92c39237a2c6e24dd7b4
Gaetandroid <citarellagaetano26@gmail.com>
2020-12-07 18:53:08 +0100

    7/12

--- es3scheda.py
+++ es3scheda.py
@@ -1,11 +1,20 @@
-print("Scrivi il testo che vuoi tradurre in rovarspraket: ")
-parola = input()
-parola = parola.lower()
-vocali = ["a", "e", "i", "o", "u", "à", "è", "ì", "ò", "ù", " "]
-p_trad = ""
-for i in range(len(parola)):
-    if parola[i] not in vocali:
-        p_trad += parola[i]+"o"+parola[i]
+ciclo = True
+while ciclo == True:
+    print("Scrivi il testo che vuoi tradurre in rovarspraket: ")
+    parola = input()
+    parola = parola.lower()
+    vocali = ["a", "e", "i", "o", "u", "à", "è", "ì", "ò", "ù", " ", ".", ",", ";", ":", "?", "!"]
+    p_trad = ""
+    for i in range(len(parola)):
+        if parola[i] not in vocali:
+            p_trad += parola[i]+"o"+parola[i]
+        else:
+            p_trad += parola[i]
+    print(p_trad)
+    print("Vuoi tradurne un altra? Digita Si per continuare, digita No per smettere")
+    risp = input()
+    rip = risp.lower
+    if risp == "si":
+        pass
     else:
-        p_trad += parola[i]
-print(p_trad)
+        ciclo = False
